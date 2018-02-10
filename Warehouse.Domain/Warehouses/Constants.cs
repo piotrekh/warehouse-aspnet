b1e4afc1d01abb0b8f26594857f1c70eb7ea3b0c
Piotr Hechelski <piotrek.h@hotmail.com>
2018-02-10 14:37:48 +0100

    Missing constants

---
+++ Warehouse.Domain/Warehouses/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace Warehouse.Domain.Warehouses
+{
+    public static class EventTypes
+    {
+        public static readonly string Import = "import";
+        public static readonly string Export = "export";
+    }
+}
