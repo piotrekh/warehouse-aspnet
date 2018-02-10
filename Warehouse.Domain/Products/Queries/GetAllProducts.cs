52c02562e097170b1cc2c94f348762b3eb90d3fc
Piotr Hechelski <piotrek.h@hotmail.com>
2018-02-10 14:43:09 +0100

    Retrieving all products

---
+++ Warehouse.Domain/Products/Queries/GetAllProducts.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using Warehouse.Domain.Common.Models;
+using Warehouse.Domain.Products.Models;
+
+namespace Warehouse.Domain.Products.Queries
+{
+    public class GetAllProducts : IRequest<ItemsResult<Product>>
+    {
+    }
+}
---
+++ Warehouse.Domain/Products/QueryHandlers/GetAllProductsHandler.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using MediatR;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Warehouse.DataAccess;
+using Warehouse.Domain.Common.Models;
+using Warehouse.Domain.Products.Models;
+using Warehouse.Domain.Products.Queries;
+
+namespace Warehouse.Domain.Products.QueryHandlers
+{
+    public class GetAllProductsHandler : IRequestHandler<GetAllProducts, ItemsResult<Product>>
+    {
+        private readonly WarehouseDbContext _dbContext;
+        private readonly IMapper _mapper;
+
+        public GetAllProductsHandler(WarehouseDbContext dbContext, IMapper mapper)
+        {
+            _dbContext = dbContext;
+            _mapper = mapper;
+        }
+
+        public Task<ItemsResult<Product>> Handle(GetAllProducts request, CancellationToken cancellationToken)
+        {
+            var entities = _dbContext.Products.ToList();
+            var products = _mapper.Map<List<Product>>(entities);
+            var result = new ItemsResult<Product>() { Items = products };
+            return Task.FromResult(result);
+        }
+    }
+}
