8873f1b37e5f421f35cbb3eb7ef7eea879d347f2
Piotr Hechelski <piotrek.h@hotmail.com>
2018-02-10 12:57:43 +0100

    Added ProductId to tables

--- Warehouse.Database/Migrations/0004_StockEvent.sql
+++ Warehouse.Database/Migrations/0004_StockEvent.sql
@@ -4,6 +4,7 @@
 	EventType varchar(20) NOT NULL,
 	EventDate datetime2(0) NOT NULL,
 	WarehouseId int NOT NULL,
+	ProductId int NOT NULL,
 	ProductName nvarchar(100) NOT NULL,
 	ProductUnit nvarchar(20) NOT NULL,
 	ProductUnitSize int NOT NULL,
@@ -11,5 +12,6 @@
 	ProductAmount int NOT NULL,
 
 	CONSTRAINT PK_StockEvent PRIMARY KEY (Id),
+	CONSTRAINT FK_StockEvent_Product_ProductId FOREIGN KEY (ProductId) REFERENCES dbo.Product (Id),
 	CONSTRAINT FK_StockEvent_Warehouse_WarehouseId FOREIGN KEY (WarehouseId) REFERENCES dbo.Warehouse (Id)
 )
--- Warehouse.Database/Migrations/0006_StockSnapshotProduct.sql
+++ Warehouse.Database/Migrations/0006_StockSnapshotProduct.sql
@@ -2,6 +2,7 @@
 (
 	Id int IDENTITY(1, 1) NOT NULL,	
 	StockSnapshotId int NOT NULL,
+	ProductId int NOT NULL,
 	ProductName nvarchar(100) NOT NULL,
 	ProductUnit nvarchar(20) NOT NULL,
 	ProductUnitSize int NOT NULL,
@@ -9,5 +10,6 @@
 	ProductAmount int NOT NULL,
 
 	CONSTRAINT PK_StockSnapshotProduct PRIMARY KEY (Id),
+	CONSTRAINT FK_StockSnapshotProduct_Product_ProductId FOREIGN KEY (ProductId) REFERENCES dbo.Product (Id),
 	CONSTRAINT FK_StockSnapshotProduct_StockSnapshot_StockSnapshotId FOREIGN KEY (StockSnapshotId) REFERENCES dbo.StockSnapshot (Id)
 )
