8dfc02845a5eca68d06404b0ac17023009b0a1c1
Piotr Hechelski <piotrek.h@hotmail.com>
2018-02-11 16:35:22 +0100

    Added endpoint for retrieving all products

---
+++ Warehouse.Api/Controllers/ProductsController.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Warehouse.Domain.Common.Models;
+using Warehouse.Domain.Products.Models;
+using Warehouse.Domain.Products.Queries;
+
+namespace Warehouse.Api.Controllers
+{
+    [RoutePrefix("api/products")]
+    public class ProductsController : ApiController
+    {
+        private readonly IMediator _mediator;
+
+        public ProductsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        [ResponseType(typeof(ItemsResult<Product>))]
+        public async Task<ItemsResult<Product>> GetAllProducts()
+        {
+            var query = new GetAllProducts();
+            return await _mediator.Send(query);
+        }
+    }
+}
